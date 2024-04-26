b37aa9ba2bc99eea892f9af2bc3e5c35db7a893a
Demir001 <109363690+D2Blade@users.noreply.github.com>
2024-04-26 17:35:28 -0300

    Add files via upload

---
+++ app_youtubeC.py
@@ -0,0 +1,25 @@
+import os
+import youtube_dl
+
+def baixar_audio(url, qualidade):
+    opcoes = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'musica/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': qualidade,
+        }],
+    }
+
+    if not os.path.exists('musica'):
+        os.makedirs('musica')
+
+    with youtube_dl.YoutubeDL(opcoes) as ydl:
+        ydl.download([url])
+
+if __name__ == "__main__":
+    link = input("Digite o link do vídeo do YouTube: ")
+    qualidade = input("Digite a qualidade desejada (ex: 128k, 192k, 256k): ")
+    baixar_audio(link, qualidade)
+
